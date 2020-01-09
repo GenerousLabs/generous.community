5c8f5d47d8c0615863982341f8ac7c6de8ca1c76
Callum Macdonald <git@callum-macdonald.com>
2020-01-09 15:00:36 +0100

    Nav links to content.

--- gatsby/src/components/navigation/content.js
+++ gatsby/src/components/navigation/content.js
@@ -1,7 +1,9 @@
 const options = [
-  { to: '/', text: 'Home' },
+  { to: "/", text: "Home" },
   // { to: '/blog/', text: 'Blog' },
-  { to: '/about/', text: 'About' },
+  { to: "/vision", text: "Vision" },
+  { to: "/retreat-format", text: "Format" },
+  { to: "/example-software", text: "Examples" }
 ];
 
 export default options;
