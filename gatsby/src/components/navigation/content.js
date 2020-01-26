e6244d903a2f39044fc36a472fd042c8cc522949
Callum Macdonald <git@callum-macdonald.com>
2020-01-26 14:22:10 +0100

    Disable other page links for now.

--- gatsby/src/components/navigation/content.js
+++ gatsby/src/components/navigation/content.js
@@ -1,9 +1,9 @@
 const options = [
-  { to: "/", text: "Home" },
+  { to: "/", text: "Home" }
   // { to: '/blog/', text: 'Blog' },
-  { to: "/vision", text: "Vision" },
-  { to: "/retreat-format", text: "Format" },
-  { to: "/example-software", text: "Examples" }
+  // { to: "/vision", text: "Vision" },
+  // { to: "/retreat-format", text: "Format" },
+  // { to: "/example-software", text: "Examples" }
 ];
 
 export default options;
