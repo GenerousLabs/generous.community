8d0a7b63e5a80b0ca2800b8b22335a1ed83018b1
Callum Macdonald <git@callum-macdonald.com>
2020-01-26 14:46:20 +0100

    Prettier.

--- gatsby/src/templates/page.js
+++ gatsby/src/templates/page.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import { Typography, Fade } from '@material-ui/core';
+import React from "react";
+import { graphql } from "gatsby";
+import { Typography, Fade } from "@material-ui/core";
 
-import { Layout, SEO, Animated } from 'components';
+import { Layout, SEO, Animated } from "components";
 
 const Page = props => {
   const { data } = props;
   const {
     frontmatter: { title, description },
-    html,
+    html
   } = data.markdownRemark;
 
   return (
     <Layout>
       <SEO title={title} description={description} />
       <Fade in timeout={1000}>
-        <Typography variant='h1' color='textPrimary'>
+        <Typography variant="h1" color="textPrimary">
           <Animated>{title}</Animated>
         </Typography>
       </Fade>
       <Typography
-        variant='body1'
+        variant="body1"
         dangerouslySetInnerHTML={{
-          __html: html,
+          __html: html
         }}
       />
     </Layout>
